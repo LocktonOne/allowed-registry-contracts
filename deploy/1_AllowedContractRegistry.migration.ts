ac62271a7cea1b2f5161d0a2144d5963eee97474
Anastasiia Lishchenko <anastasiia.lishchenko@gmail.com>
2024-09-03 16:38:54 +0300

    Add registry to master contract registry

--- deploy/1_AllowedContractRegistry.migration.ts
+++ deploy/1_AllowedContractRegistry.migration.ts
@@ -1,9 +1,15 @@
-import { Deployer, Reporter } from "@solarity/hardhat-migrate";
+import { Deployer } from "@solarity/hardhat-migrate";
 
-import { AllowedContractRegistry__factory } from "@ethers-v6";
+import { AllowedContractRegistry__factory, MasterContractsRegistry__factory } from "@ethers-v6";
+import { getConfigJsonFromVault } from "./config/config-getter";
 
 export = async (deployer: Deployer) => {
-  await deployer.deploy(AllowedContractRegistry__factory, {
+  const config = await getConfigJsonFromVault();
+
+  const registry = await deployer.deployed(MasterContractsRegistry__factory, config.addresses.MasterContractsRegistry);
+
+  const allowedContractRegistry = await deployer.deploy(AllowedContractRegistry__factory, {
     name: "AllowedContractRegistry",
   });
+  await registry.addProxyContract("ALLOWED_CONTRACT_REGISTRY", await allowedContractRegistry.getAddress());
 };
